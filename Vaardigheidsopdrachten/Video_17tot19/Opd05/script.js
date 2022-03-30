dfb139a2c0109209ce8c9341c83eca96110c89fc
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-03-30 11:12:16 +0200

    Opd 05

---
+++ Vaardigheidsopdrachten/Video_17tot19/Opd05/script.js
@@ -0,0 +1,5 @@
+const firstName = "Storm";
+const lastName = "Leeuwin"
+const age = 16;
+
+console.log(`Mijn naam is ${firstName} en ik ben ${age} jaar oud. Daarnaast is mijn familienaam ${lastName}`);
