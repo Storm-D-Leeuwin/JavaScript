28146aae075b61504458f5ce52de6ea1acc968da
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-03-30 11:24:39 +0200

    Opd 6 t/m 9

---
+++ Vaardigheidsopdrachten/Video_17tot19/Opd06_Opd09/script.js
@@ -0,0 +1,39 @@
+//Opdracht 6
+
+regent = true;
+
+if (regent) {
+    console.log("Ik neem mijn paraplu mee naar buiten.");
+} else {
+    console.log("Ik zet mijn zonnebril op.");
+}
+
+//Opdracht 7
+
+grasGeel = true;
+
+if (grasGeel) {
+    console.log("Ik geef het gras water.");
+} else {
+    console.log("Ik doe niks.");
+}
+
+//Opdracht 8
+
+winter = false;
+
+if (winter) {
+    console.log("Ik zet de verwarming aan.");
+} else {
+    console.log("Ik dans op tafel.");
+}
+
+//Opdracht 9
+
+regennen = false;
+
+if (regennen) {
+    console.log("Ik doe een regenpak aan.");
+} else {
+    console.log("Ik doe mijn zonnebril op.");
+}
