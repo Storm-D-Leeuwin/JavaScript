aa4f4757f00eecf324f26c854d687a1c24dc9280
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-01 21:00:48 +0200

    Opd 16 /m 19

---
+++ Vaardigheidsopdrachten/Video_20tot25/Opd16_17_18_19/script.js
@@ -0,0 +1,27 @@
+//Opdracht 16
+
+const age = 19;
+
+if (age >= 18) {
+    console.log("Opdracht 16: Whieee stufi!");
+} else {
+    console.log("Opdracht 16: Helaas geen studiefinanciering");
+}
+
+//Opdracht 17
+
+primaireVerzorging = false;
+
+if (primaireVerzorging = false) {
+    console.log("Opdracht 17: Whieee veel stufi!");
+} else {
+    console.log("Opdracht 17: Helaas geen studiefinanciering");
+}
+
+//Opdracht 18
+
+console.log(`Opdracht 18: Wel`);
+
+//Opdracht 19
+
+console.log(`Opdracht 19: Wel`);
