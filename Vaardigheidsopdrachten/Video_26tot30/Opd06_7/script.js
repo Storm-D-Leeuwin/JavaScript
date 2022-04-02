4acb216b06b3cfb5c2cc20a47dae17d1f7aec080
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-02 11:48:39 +0200

    Opd 6 t/m 7

---
+++ Vaardigheidsopdrachten/Video_26tot30/Opd06_7/script.js
@@ -0,0 +1,32 @@
+//Opdracht 6
+
+const job = "teacher";
+
+switch (job) {
+    case "developer":
+        console.log("LIKE!");
+break;
+    case "teacher":
+    case "consultant":
+        console.log("Nice...");
+break;
+    default:
+        console.log("I don't know...");
+}
+
+//Opdracht 7
+
+const job2 = "retired";
+
+switch (job2) {
+    case "developer":
+    case "retired":
+        console.log("LIKE!");
+break;
+    case "teacher":
+    case "consultant":
+        console.log("Nice...");
+break;
+    default:
+        console.log("I don't know...");
+}
