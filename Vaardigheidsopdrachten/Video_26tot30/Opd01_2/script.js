6db76bf908ce906c5b2223161ea5f1990ec28693
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-02 11:28:41 +0200

    Opd 1 & 2

---
+++ Vaardigheidsopdrachten/Video_26tot30/Opd01_2/script.js
@@ -0,0 +1,21 @@
+//Opdracht 1 & 2
+
+const dag = 'zondag';
+
+switch(dag){
+    case 'maandag':
+    case 'vrijdag':
+    case 'donderdag':
+        console.log(`Hij eet falafel met salade`);
+break;
+    case 'dinsdag':
+    case 'woensdag':
+        console.log(`Hij eet spinazie`);
+break;
+    case 'zaterdag':
+    case 'zondag':
+        console.log(`Hij eet linzensoep`);
+break;
+    case 'kerst':
+        console.log('Hij eet kalkoen');
+    }
