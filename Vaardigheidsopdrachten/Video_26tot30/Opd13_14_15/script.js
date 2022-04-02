e6a535a05415f665faea895d2dafd01644d696ee
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-02 13:08:20 +0200

    Opd 13 t/m 15

---
+++ Vaardigheidsopdrachten/Video_26tot30/Opd13_14_15/script.js
@@ -0,0 +1,17 @@
+//Opdracht 13
+
+let city = 'Amsterdam';
+
+city === 'Amsterdam' ? console.log("Koetjekoe") : console.log(`Deze stad is practig is het niet: ${city}`); 
+
+//Opdracht 14
+
+let city2 = 'Amsterdam';
+
+city2 === 'Amsterdam' ? console.log("Koetjekoe") : console.log("Koetjekoe");
+
+//Opdracht 15
+
+let city3 = 'Amsterdam';
+
+city3 === 'Amsterdam' && true ? console.log("Koetjekoe") : console.log("Koetjeboeboe") 
