59fda84dfb2e2eb20c257f2b2bd87507948121b4
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-02 12:56:45 +0200

    Opd 11 & 12

---
+++ Vaardigheidsopdrachten/Video_26tot30/Opd11_12/script.js
@@ -0,0 +1,18 @@
+//Opdracht 11
+
+let sentence;
+if (sentence = false) {
+    console.log('Whieee');
+} else {
+    console.log('Whaaa');
+}
+
+//Opdracht 12
+
+if (sentence = false) {
+    sentence = 'Whieee'
+} else {
+    sentence = 'Whaaa'
+}
+
+console.log(`Hello, i shout out the sentence ${sentence} and I am ${sentence.length > 5 ? 'Happy' : 'Not so happy' }`);
