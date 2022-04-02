f0235d8288ec210a749eba49976eec565a02a508
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-02 11:42:29 +0200

    Opd 3 t/m 5

---
+++ Vaardigheidsopdrachten/Video_26tot30/Opd03_4_5/script.js
@@ -0,0 +1,31 @@
+//Opdracht 3
+
+const car = "Audi";
+
+if (car == "Audi") {
+    console.log("Great!");
+} else if (car == "Mercedes") {
+    console.log("Bit great!");
+} else if (car == "BMW") {
+    console.log("Hmm...");
+} else if (car == "Opel") {
+    console.log("OK?");
+} else {
+    console.log("Not so great!");
+}
+
+//Opdracht 4 & 5
+
+const car2 = "Porsche"
+
+if (car2 == "Audi" || car2 == "Porsche") {
+    console.log("Great!");
+} else if (car2 == "Mercedes") {
+    console.log("Bit great!");
+} else if (car2 == "BMW") {
+    console.log("Hmm...");
+} else if (car2 == "Opel") {
+    console.log("OK?");
+} else {
+    console.log("Not so great!");
+}
