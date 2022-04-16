2de7d08ee998a39f3e1c002c6ba75d90119648bb
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-16 16:11:06 +0200

    Opd 6

---
+++ Week3/Vaardigheidsopdrachten/01_video31_38/Opd06/script.js
@@ -0,0 +1,11 @@
+function som (one, two) {
+    if (one > two){
+        console.log(`${one} is groter dan ${two}`);
+    } else if (two > one) {
+        console.log(`${two} is groter dan ${one}`);
+    }
+}
+
+console.log(som(12,4));
+console.log(som(5,7));
+
