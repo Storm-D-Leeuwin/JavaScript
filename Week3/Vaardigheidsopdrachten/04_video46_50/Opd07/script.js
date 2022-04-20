7582f5497d503b7ddffb10d1afa1b666ea09d3b8
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-20 20:01:21 +0200

    Opd 7

--- Week3/Vaardigheidsopdrachten/04_video46_50/Opd07/script.js
+++ Week3/Vaardigheidsopdrachten/04_video46_50/Opd07/script.js
@@ -1,9 +1,9 @@
-for (let i = 2; i <= 1000; i + 2) {
+for (let i = 2; i <= 1000; i = i + 2) {
     console.log(`For: ${i}`);
 }
 
 let i = 2;
 while(i <= 1000) {
     console.log(`While: ${i}`)
-    i + 2;
+    i = i + 2;
 }
