af777a57e0277f8c9b2f8607c33a91adef1bd0b3
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-20 18:37:56 +0200

    Opd 5

---
+++ Week3/Vaardigheidsopdrachten/03_video42_45/Opd05/script.js
@@ -0,0 +1,8 @@
+const objectOne = {
+    firstName: 'Piet',
+    lastName: 'Janssens',
+    job: 'Fighter',
+};
+
+console.log(`My name is ${objectOne.firstName} ${objectOne.lastName} and I am a ${objectOne.job}`);
+
