8808a68d8b3a6c7a9e216ecdf4fb91f5013e04aa
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-21 13:11:31 +0200

    Opd 23

---
+++ Week3/Vaardigheidsopdrachten/04_video46_50/Opd23/script.js
@@ -0,0 +1,9 @@
+for(let part = 1; part <= 5; part++){
+    console.log(`Let's start part ${part}!`);
+    let subpart = 0;
+    while (subpart < 5) {
+        console.log(subpart);
+        subpart++;
+        part++;
+    }
+}
