d23f447b20d18ff14c939a0410b9d19d9c2d63b1
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-21 08:59:53 +0200

    Opd 16

---
+++ Week3/Vaardigheidsopdrachten/04_video46_50/Opd16/script.js
@@ -0,0 +1,10 @@
+const arrayOne = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+const array = arrayOne[0] + arrayOne[1] + arrayOne[2] + arrayOne[3] + arrayOne[4] + arrayOne[5] + arrayOne[6] + arrayOne[7] + arrayOne[8] + arrayOne[9] + arrayOne[10] + arrayOne[11] + arrayOne[12] + arrayOne[13] + arrayOne[14];
+
+if(array > 100) {
+   console.log('Array som is groter dan 100')
+} else if (array < 100) {
+   console.log(array * 2);
+}
+
