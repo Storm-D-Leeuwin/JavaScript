1db0d292a5c91f590e7850c75b82f7adc341a226
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-25 09:33:47 +0200

    Opd 6

---
+++ Week5/Vaardigheidsopdrachten/Sectie7/Opd06/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+document.querySelector('div').style.height = 'min-content';
+document.querySelector('div').style.textAlign = 'center';
+document.querySelector('body').style.margin = '0';
+document.querySelector('body').style.padding = '0';
+
+const d = document.querySelector('.item-1');
+
+d.textContent = 'H';
+d.style.backgroundColor = 'red';
+
+const e = document.querySelector('.item-2');
+
+e.style.backgroundColor = 'blue';
+
+const f = document.querySelector('.item-3');
+
+f.style.backgroundColor = 'green';
+
+const g = document.querySelector('.item-4');
+
+g.style.backgroundColor = 'purple';
+
+const h = document.querySelector('.item-5');
+
+h.style.backgroundColor = 'brown';
+
+const i = document.querySelector('.item-6');
+
+i.style.backgroundColor = 'pink';
+
+const j = document.querySelector('.item-7');
+
+j.style.backgroundColor = 'yellow';
