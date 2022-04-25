db8b28a865b70a9bd058c89796e86aa19110f235
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-25 09:37:21 +0200

    Opd 8

---
+++ Week5/Vaardigheidsopdrachten/Sectie7/Opd08/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+document.querySelector('div').style.height = 'min-content';
+document.querySelector('div').style.textAlign = 'center';
+document.querySelector('body').style.margin = '0';
+document.querySelector('body').style.padding = '0';
+
+const d = document.querySelector('.item-1');
+
+d.textContent = 'Konijntje';
+d.style.backgroundColor = 'red';
+
+const e = document.querySelector('.item-2');
+
+e.textContent = 'Konijntje';
+e.style.backgroundColor = 'blue';
+
+const f = document.querySelector('.item-3');
+
+f.textContent = 'Konijntje';
+f.style.backgroundColor = 'green';
+
+const g = document.querySelector('.item-4');
+
+g.textContent = 'Konijntje';
+g.style.backgroundColor = 'purple';
+
+const h = document.querySelector('.item-5');
+
+h.textContent = 'Konijntje';
+h.style.backgroundColor = 'brown';
+
+const i = document.querySelector('.item-6');
+
+i.textContent = 'Konijntje';
+i.style.backgroundColor = 'pink';
+
+const j = document.querySelector('.item-7');
+
+j.textContent = 'Konijntje';
+j.style.backgroundColor = 'yellow';
