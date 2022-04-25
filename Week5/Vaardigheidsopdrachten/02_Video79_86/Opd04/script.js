a472dec49c52ca92594c79fc2d60ad1cb4b723d5
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-25 14:20:02 +0200

    Opd 4

---
+++ Week5/Vaardigheidsopdrachten/02_Video79_86/Opd04/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const div = document.querySelectorAll('.item');
+
+for (let i = 0; i < 3; i++){
+div[i].addEventListener('click', function () {
+   console.log(div[i].textContent);
+})
+}
