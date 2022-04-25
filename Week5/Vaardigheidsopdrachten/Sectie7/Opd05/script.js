583d62381be5b5e420b223d58228edf2297df2c0
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-04-25 09:20:21 +0200

    Opd 5

---
+++ Week5/Vaardigheidsopdrachten/Sectie7/Opd05/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const een = document.querySelector('.item-1').textContent;
+const twee = document.querySelector('.item-2').textContent;
+const drie = document.querySelector('.item-3').textContent;
+const vier = document.querySelector('.item-4').textContent;
+
+console.log(een, twee, drie, vier);
