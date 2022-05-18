230ab4487c2f80151558a9277645d4e6a407ac08
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-05-18 14:17:48 +0200

    Toets / Opd 9

---
+++ toets/Opd09/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let container = document.querySelector('.container');
+let nummer = 0;
+
+let array = [];
+
+for (let i = 0; i < 50; i++) {
+    let j = Math.floor(Math.random() * 900 + 100);
+    array.push(j);
+}
+
+document.querySelector('.button').addEventListener('click', () => {
+
+    if (nummer < 49) {
+        for (let k = 0; k < 49; k++) {
+        nummer++;
+        let div = document.createElement('div');
+        div.classList.add('container__item');
+        div.innerHTML = array[k];
+        container.appendChild(div);
+        }
+    }
+
+
+})
+
+
+
